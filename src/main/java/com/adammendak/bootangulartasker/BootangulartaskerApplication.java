02c6942aea467a429784497c7c35da8eaa108884
adammendak <adam.mendak@gmail.com>
2017-11-24 17:32:49 +0100

    added src

---
+++ src/main/java/com/adammendak/bootangulartasker/BootangulartaskerApplication.java
@@ -0,0 +1,12 @@
+package com.adammendak.bootangulartasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootangulartaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootangulartaskerApplication.class, args);
+	}
+}
